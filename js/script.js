a7a8325127286d54221c02ce49d39bfb7c399dbb
Laryssa Souza Ramos <laryssaramos284@gmail.com>
2022-08-23 19:02:35 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let closePlayer = document.querySelector('#close-player');
+let musicPlayer = document.querySelector('.music-player'); 
+let sectionPlayer = document.querySelector('.last')
+let music = document.querySelector('#audio')
+
+let src = music.getAttribute('data-src');
+
+musicPlayer.querySelector('audio').src = src;
+musicPlayer.querySelector('audio').play();
+
+
+
+closePlayer.onclick =() =>{ 
+
+    closePlayer.classList.toggle('fa-times'); 
+    musicPlayer.classList.toggle('active'); 
+    sectionPlayer.classList.toggle('active'); 
+}
+
